681c63927639a75196b380826421859047b2d439
Leandro Luis <leandro.luis@uptc.edu.co>
2025-11-07 23:04:32 -0500

    inicializacion proyecto

---
+++ src/main/java/co/edu/uptc/notification/NotificationQuejasBoyacaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationQuejasBoyacaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationQuejasBoyacaApplication.class, args);
+    }
+
+}
